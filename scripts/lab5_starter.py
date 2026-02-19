31c971966db4198d1cf192f40e4ef2ff57bcbac9
Amisha Nambiar <64343290+amishana@users.noreply.github.com>
2026-02-19 12:12:47 -0800

    Update lab5_starter.py

--- scripts/lab5_starter.py
+++ scripts/lab5_starter.py
@@ -170,7 +170,7 @@ class GoalPositionController:
             else:
                 ctrl_msg.linear.x = 0.2
     
-            if abs(angle_error) > 0.5:
+            if abs(angle_error) > 0.05:
                 # ctrl_msg.linear.x = 0.0
                 ctrl_msg.angular.z = self.p_rot.control(angle_error, t)
             else:
