54940f6cafbcf3de2b828b7107f193affda280a9
Amisha Nambiar <64343290+amishana@users.noreply.github.com>
2026-02-26 10:50:38 -0800

    Update lab5_starter.py

--- scripts/lab5_starter.py
+++ scripts/lab5_starter.py
@@ -117,7 +117,8 @@ class GoalPositionController:
 
         # define PID controllers for linear and angular velocities
         ######### Your code starts here #########
-        self.p_rot = PIDController(2.0, 0.03, 0.01, 0.0, -1.5, 1.5)
+        # self.p_rot = PIDController(2.0, 0.03, 0.01, 0.0, -1.5, 1.5)
+        self.p_rot = PDController(2.0, 0.03, 0.01, -1, 1)
 
         ######### Your code ends here #########
 
@@ -199,7 +200,8 @@ class GoalAngleController:
 
         # define PID controller angular velocity
         ######### Your code starts here #########
-        self.p_rot = PIDController(2.0, 0.03, 0.01, 0.0, -1, 1)
+        # self.p_rot = PIDController(2.0, 0.03, 0.01, 0.0, -1, 1)
+        self.p_rot = PDController(2.0, 0.03, 0.01, -1, 1)
 
         ######### Your code ends here #########
 
